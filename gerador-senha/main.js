aaf49715d5ec7a33cfe2e4bc365407639a5bd6db
Your Name <luanarabuta@gmail.com>
2025-01-23 16:05:32 -0300

    gerador senha

--- gerador-senha/main.js
+++ gerador-senha/main.js
@@ -1,20 +1,4 @@
 const { app, BrowserWindow } = require('electron')
-const Store = require('electron-store');
-
-const schema = {
-	foo: {
-		type: 'number',
-		maximum: 100,
-		minimum: 1,
-		default: 50
-	},
-	bar: {
-		type: 'string',
-		format: 'url'
-	}
-};
-
-const store = new Store({schema});
 
 const createWindow = () => {
   const win = new BrowserWindow({
